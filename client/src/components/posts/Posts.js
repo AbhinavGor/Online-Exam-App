2efda2e9d1777ad45bac6415ccb4fe3941e98d3b
AbhinavGor <56104674+AbhinavGor@users.noreply.github.com>
2020-05-08 17:55:53 +0530

    Questions section POST error rectified

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -4,21 +4,11 @@ import { connect } from 'react-redux';
 import PostItem from './PostItem';
 import PostForm from './PostForm';
 import { getPosts } from '../../actions/post';
-import { addPost } from '../../actions/post';
 
 const Posts = ({ getPosts, post: { posts } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  // var score = 0;
-
-  // function check(answer, key){
-  //   if (key === answer){
-  //     document.getElementById(answer).innerHTML = 'Correct';
-  //     // document.getElementById('posts').style.visibility = "none";
-  //     score++;
-  //   }
-  // }
   return (
     <Fragment>
       <PostForm />
@@ -27,74 +17,6 @@ const Posts = ({ getPosts, post: { posts } }) => {
           <PostItem key={post._id} post={post} />
         ))}
       </div>
-      {/* <div className='post-form'>
-      <form
-        className='form my-1'
-        onSubmit={e => {
-          e.preventDefault();
-          addPost({ ans4 });
-          setText4('');
-          checkall(ans1, ans2, ans3, ans4);
-        }}
-      >
-         <div className='bg-primary p'>
-          <h3>Question 1</h3>
-        </div>
-        <h1>What is 1+2?</h1>
-        <div id='3'><input
-          id='ans1'
-          name='text'
-          cols='30'
-          rows='5'
-          placeholder='Answer'
-          value={ans1}
-          onChange={e => setText(e.target.value)}
-          
-        /><br /></div>
-        <div className='bg-primary p'>
-          <h3>Question 2</h3>
-        </div>
-        <h1>New Delhi is capital of ______?</h1>
-        <div id='india'><input
-          name='text'
-          cols='30'
-          rows='5'
-          placeholder='Answer'
-          value={ans2}
-          onChange={e => setText2(e.target.value)}
-          
-        /><br /></div>
-        <div className='bg-primary p'>
-          <h3>Question 3</h3>
-        </div>
-        <h1>Waht is the speeling of speling?</h1>
-        <div id='spelling'><input
-          name='text'
-          cols='30'
-          rows='5'
-          placeholder='Answer'
-          value={ans3}
-          onChange={e => setText3(e.target.value)}
-          
-        /><br /></div>
-        <div className='bg-primary p'>
-          <h3>Question 4</h3>
-        </div>
-        <h1>Am I creepy?</h1>
-        <div id='No'><input
-          name='text'
-          cols='30'
-          rows='5'
-          placeholder='Answer'
-          value={ans4}
-          onChange={e => setText4(e.target.value)}
-          
-        /><br />
-        <input type='submit' className='btn btn-dark my-1' value='Submit'/></div>
-      </form>
-      
-      <div id='ans'>{score}</div>
-    </div> */}
     </Fragment>
   );
 };
