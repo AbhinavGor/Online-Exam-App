0dfc88c9681f0060b7cc909b66361687a71877e0
AbhinavGor <56104674+AbhinavGor@users.noreply.github.com>
2020-05-17 19:23:36 +0530

    Changed production DB

--- models/Post.js
+++ models/Post.js
@@ -57,37 +57,6 @@ const PostSchema = new Schema({
     avatar:{
         type: String
     },
-    likes:[
-        {
-            user: {
-                type: Schema.Types.ObjectId,
-                ref:'users'
-            }
-        }
-    ],
-    comments: [
-        {
-        user: {
-            type: Schema.Types.ObjectId,
-            ref:'users'
-        },
-        text:{
-            type: String,
-            required: true
-        },
-        name:{
-            type:String,
-        },
-        avatar:{
-            type: String
-        },
-        date:{
-            type: Date,
-            default: Date.now,
-        }
-    }
-    ],
-
     date:{
         type: Date,
         default: Date.now,
